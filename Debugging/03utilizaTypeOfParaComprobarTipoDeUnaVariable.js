110ed5ae6794d1ed8f1fd0ad6e8c96a8c322ec5b
GerLoaiza03 <german.loaiza03@gmail.com>
2022-05-10 12:38:55 -0500

    Terminando Debbuging FreeCodeCamp

---
+++ Debugging/03utilizaTypeOfParaComprobarTipoDeUnaVariable.js
@@ -0,0 +1,30 @@
+/*
+Puedes utilizar typeof para comprobar la estructura de datos, o tipo, de una variable. Esto es útil en la depuración cuando se trabaja con múltiples tipos de datos. Si crees que estás sumando dos números, pero uno es en realidad una cadena, los resultados pueden ser inesperados. Los errores de tipo pueden estar al acecho en los cálculos o en las llamadas a funciones. Ten cuidado especialmente cuando accedas y trabajes con datos externos en forma de objeto de JavaScript Object Notation (JSON).
+
+Aquí hay algunos ejemplos que utilizan typeof:
+
+console.log(typeof "");
+console.log(typeof 0);
+console.log(typeof []);
+console.log(typeof {});
+En orden, la consola mostrará las cadenas string, number, object, y object.
+
+JavaScript reconoce siete tipos de datos primitivos (inmutables): Boolean, Null, Undefined, Number, String, Symbol (nuevo con ES6), y BigInt (nuevo con ES2020), y un tipo para elementos mutables: Object. Ten en cuenta que en JavaScript, los arreglos son técnicamente un tipo de objeto.
+
+Agrega dos sentencias console.log() para comprobar el typeof de cada una de las dos variables seven y three en el código.
+*/
+
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Cambia solo el código debajo de esta línea
+console.log(typeof seven);
+console.log(typeof three);
+
+/*PRUEBAS
+Tu código debe utilizar typeof en dos sentencias console.log() para comprobar el tipo de las variables.
+
+Tu código debe utilizar typeof para comprobar el tipo de la variable seven.
+
+Tu código debe utilizar typeof para comprobar el tipo de la variable three.
+*/
