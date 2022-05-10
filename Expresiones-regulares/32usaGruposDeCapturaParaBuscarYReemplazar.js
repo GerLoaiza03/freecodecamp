9bbfd615c3ab604a68fa16d3596698ae2b9e5404
GerLoaiza03 <german.loaiza03@gmail.com>
2022-05-10 11:56:19 -0500

    Terminando RegExp FreeCodeCamp

--- Expresiones-regulares/32usaGruposDeCapturaParaBuscarYReemplazar.js
+++ Expresiones-regulares/32usaGruposDeCapturaParaBuscarYReemplazar.js
@@ -16,7 +16,15 @@ La llamada replace devolverá la cadena Camp Code.
 Escribe una expresión regular fixRegex utilizando tres grupos de captura que buscarán cada palabra en la cadena one two three. Luego actualiza la variable replaceText para reemplazar one two three con la cadena three two one y asigna el resultado a la variable result. Asegúrate de utilizar grupos de captura en la cadena de reemplazo utilizando la sintaxis del signo de dólar ($).
 */
 
+let str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Cambia esta línea
+let replaceText = "$3 $2 $1"; // Cambia esta línea
+let result = str.replace(fixRegex, replaceText);
 
+console.log(fixRegex)
+console.log(replaceText)
+console.log(str)
+console.log(result)
 
 /*PRUEBAS
 Debes utilizar .replace() para buscar y reemplazar.
